2fb5ac5a5317904f208ef928587fd328a5c5c935
mike <mike.fch1@gmail.com>
2025-05-30 08:27:28 +0300

    add generated

---
+++ Readme.md
@@ -0,0 +1,14 @@
+
+# pumpfun-dsl-go
+
+create init/buy/sell transaction in go, without pain.
+
+
+
+### regenerate models
+
+```sh
+sh generate.sh
+```
+
+
---
+++ generate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf solana-anchor-go
+rm -rf pump-fun.json
+
+wget https://raw.githubusercontent.com/rckprtr/pumpdotfun-sdk/refs/heads/main/src/IDL/pump-fun.json 
+
+git clone https://github.com/fragmetric-labs/solana-anchor-go
+
+cd solana-anchor-go
+
+go mod tidy
+
+go build .
+
+cd ..
+
+./solana-anchor-go/solana-anchor-go -dst pump -src pump-fun.json
